0a6848506959410929af90a9227b38ece98d0c20
Vishal Dogra <dogravishal1598@gmail.com>
2024-03-09 21:25:29 +0530

    project setup and added dependencies

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return <div>Github Finder</div>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
